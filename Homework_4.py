4c1f1444734fc50f274e2daab5cea72ba93114d1
Videnin <videningeorg@gmail.com>
2023-04-03 17:59:11 +0300

    Homework4_Videnin

---
+++ Homework_4.py
@@ -0,0 +1,13 @@
+#Запросить у пользователя два целых числа.
+
+#Вывести на одной строке выражение их суммы, на второй выражение для произведения:
+
+first_number = int(input( "Enter first number : "))
+second_number = int(input( "Enter second number: "))
+
+
+sum = first_number + second_number
+multiply = first_number * second_number
+
+print(f"{first_number} + {second_number} = {sum}")
+print(f"{first_number} * {second_number} = {multiply}")
