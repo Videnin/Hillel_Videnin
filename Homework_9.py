baddd6a0935c66da214af6e8699563761ad883dd
Videnin <videningeorg@gmail.com>
2023-04-05 10:52:16 +0300

    Homework3_Videnin Rework

--- Homework_9.py
+++ Homework_9.py
@@ -1,5 +1,6 @@
-n = 4
+n = int(input("рандомное число: "))
+
 for i in range(1, n+1):
-    print(' '.join(str(j) for j in range(i, i*2)))
+    print(" ".join(str(j) for j in range(i, 0, -1)) + " " + " ".join(str(j) for j in range(2, i+1)))
 for i in range(n-1, 0, -1):
-    print(' '.join(str(j) for j in range(i, i*2)))
+    print(" ".join(str(j) for j in range(i, 0, -1)) + " " + " ".join(str(j) for j in range(2, i+1)))
