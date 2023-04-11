c99ce01251da8beaf3d24d0a90bdda076a05dddf
Videnin <videningeorg@gmail.com>
2023-04-11 19:06:52 +0300

    Homework 11

--- Homework_11.py
+++ Homework_11.py
@@ -1,8 +1,13 @@
-multi_string = "Hello all. Here's pretty cold and hot. Choose yourself"
+multi_string = "Hello all. Here's pretty cold and hot. Choose yourself."
+multi_string = multi_string.rstrip('.')
 sentences = multi_string.split('.')
-word_counts = []
+words_number = []
+
 for sentence in sentences:
-    words = sentence.strip().split(' ')
-    word_counts.append(len(words))
-print(word_counts)
+    words = sentence.strip().split()
+    words_number.append(len(words))
+
+print(words_number)
+
+
 
