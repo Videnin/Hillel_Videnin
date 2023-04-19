6177d8d9532eacb3d6cd67a110671e0ccea1d557
Videnin <videningeorg@gmail.com>
2023-04-19 18:31:29 +0300

    Homework 14

--- Homework_)21.py
+++ Homework_)21.py
@@ -1,34 +1,31 @@
-
-
 import json
 
-users = []
-with open('users_in.txt', 'r') as f:
-    for line in f:
-        # Удаляем символы переноса строки и разделяем строку по точке с запятой
-        data = line.strip().split(';')
-        name = data[0].strip()
-        # Пытаемся преобразовать возраст в целое число, если не получается - присваиваем None
-        try:
-            age = int(data[1].strip())
-        except (ValueError, IndexError):
-            age = None
-        # Создаем список телефонов, удаляя лишние пробелы
-        phones = [phone.strip() for phone in data[2].split(',')]
-        # Создаем словарь пользователя
-        user = {'name': name, 'age': age, 'phones': phones}
-        # Добавляем словарь в список пользователей
-        users.append(user)
+result = []
 
-# Записываем список пользователей в JSON-файл
-with open('users_out.json', 'w') as f:
-    json.dump(users, f)
+with open('users.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+        fields = line.split(';')
+        name = fields[0].strip()
+        age = int(fields[1]) if fields[1].isdigit() else None
+        phones = [phone.strip() for phone in fields[2].split(',')] if len(fields) > 2 else []
 
-# Записываем список пользователей в текстовый файл
-with open('users_out.txt', 'w') as f:
-    for user in users:
-        # Преобразуем возраст и список телефонов в пустую строку, если они не указаны
-        age = str(user['age']) if user['age'] is not None else ''
-        phones = ','.join(user['phones']) if user['phones'] else ''
-        # Записываем данные в файл, разделяя точкой с запятой
-        f.write(f"{user['name']};{age};{phones}\n")
+        user_dict = {
+            'name': name,
+            'age': age,
+            'phones': phones
+        }
+        result.append(user_dict)
+
+with open('users_out.json', 'w') as json_file:
+    json.dump(result, json_file)
+
+# Сохранение в TXT файл
+with open('users_out.txt', 'w') as txt_file:
+    for user in result:
+        name = user['name']
+        age = user['age'] if user['age'] is not None else ''
+        phones = ', '.join(user['phones']) if len(user['phones']) > 0 else ''
+        txt_file.write(f"{name};{age};{phones}\n")
