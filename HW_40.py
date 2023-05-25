ca4323dae14b02c245db993b5881dc5dcb75a0ae
Videnin <videningeorg@gmail.com>
2023-05-25 08:23:25 +0300

    Homework 14

---
+++ HW_40.py
@@ -0,0 +1,39 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.elapsed_time = 0
+        self.start_time = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.elapsed_time += time.time() - self.start_time
+        self.start_time = None
+
+    def reset(self):
+        self.elapsed_time = 0
+
+
+# Пример использования
+
+with Timer() as t:
+    time.sleep(1)
+print(t.elapsed_time)  # ~1 second
+
+time.sleep(1)
+
+with t:
+    time.sleep(2)
+print(t.elapsed_time)  # ~3 seconds
+
+with Timer() as t2:
+    time.sleep(1)
+print(t2.elapsed_time)  # ~1 second
+
+t2.reset()
+with t2:
+    time.sleep(2)
+print(t2.elapsed_time)  # ~2 seconds
