5e4d6d078141669163d761b583d91ffed3c4da1b
Videnin <videningeorg@gmail.com>
2023-05-25 08:31:04 +0300

    Homework 14

--- Homework_9.py
+++ Homework_9.py
@@ -1,10 +1,23 @@
-n = int(input("Enter n: "))
-max_num = n*2-1
+def print_number_pattern(n):
+    # Определение максимального числа в строке
+    max_num = n
 
-for i in range(1, n+1):
-    print(" "*(n-i), end="")
-    for j in range(1, i+1):
-        print(j, end=" ")
-    for j in range(i-1, 0, -1):
-        print(j, end=" ")
-    print()
+    # Формирование паттерна чисел
+    pattern = []
+    for i in range(1, n + 1):
+        line = list(range(1, i + 1)) + list(range(i - 1, 0, -1))
+        pattern.append(line)
+
+    # Определение максимальной длины строки
+    max_length = len(' '.join(map(str, pattern[-1])))
+
+    # Вывод паттерна чисел
+    for line in pattern:
+        # Центрирование строки
+        line_str = ' '.join(map(str, line)).center(max_length)
+
+        print(line_str)
+
+
+n = int(input("Введите число n: "))
+print_number_pattern(n)
