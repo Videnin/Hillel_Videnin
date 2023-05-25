3e980e231422b5a526a6c07cf9f5c41a0b9dd68f
Videnin <videningeorg@gmail.com>
2023-05-25 08:32:40 +0300

    Homework 14

--- HW_28.py
+++ HW_28.py
@@ -1,5 +1,18 @@
-def generate_sentence(text: str) -> str:
-    sentences = [s.strip() for s in re.split(r'(?<=[.?!])\s+', text) if s.strip()]
-    first_words = [s.split()[0].lower() for s in sentences]
-    result = ' '.join(first_words)
-    return result.capitalize() + '.'
+import re
+
+def construct_sentence(text):
+    sentence_regex = r'[A-Z][^.!?]*[.!?]'
+    sentences = re.findall(sentence_regex, text)
+    words = [sentence.split(',')[0].strip().lower() for sentence in sentences]
+
+    # Составляем итоговое предложение
+    first_word = words[0].capitalize()
+    rest_of_sentence = ' '.join(words[1:])
+    final_sentence = f"{first_word} {rest_of_sentence}."
+
+    return final_sentence
+
+# Пример использования
+text = "Это первое предложение. второе предложение, третье предложение. И последнее предложение!"
+result = construct_sentence(text)
+print(result)
